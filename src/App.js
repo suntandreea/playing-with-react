9a4e4f779e17c24fe7bee313663f5863b2b30f24
Andreea Cozariuc <suntandreea@protonmail.com>
2022-02-07 00:10:04 +0200

    all meetups & add new meetup, with firebase to POST and GET data

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { Route, Switch } from 'react-router-dom';
+
+import AllMetupsPage from './pages/AllMetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
+import Layout from './components/layout/Layout';
+
+function App() {
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <AllMetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </Layout>
+  );
+}
+
+export default App;
